af82de43fd313e06afb56ecf1232476165f9198a
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-13 18:39:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,10 +12,12 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
   def brand=(brand)
     @brand = brand
     if BRANDS.include?(brand)
       BRANDS << brand
     end
   end
+
 end
